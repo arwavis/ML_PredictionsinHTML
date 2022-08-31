243619131dea9c318def890a926fe30114e6a58e
arwavis <v_6arav@hotmail.com>
2022-08-31 18:10:34 +0530

    commit 31082022

--- Smoke_Detection/smoke_app.py
+++ Smoke_Detection/smoke_app.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
 
-
 app = Flask(__name__)
 
 
 @app.route("/")
 def root_page():
     return render_template('index.html')
+
+
+@app.route("/predict")
+def form_page():
+    return render_template('forms.html')
 
 
 if __name__ == "__main__":
